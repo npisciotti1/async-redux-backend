30ff3a16d471e089e933963029029e027d7d8964
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-19 16:03:06 -0700

    spelling fix for PUT route

--- route/task-router.js
+++ route/task-router.js
@@ -6,7 +6,7 @@ const taskRouter = module.exports = new require('express').Router();
 const Task = require('../model/task.js');
 
 taskRouter.post('/api/tasks', jsonParser, (req, res, next) => {
-  console.log('/api/tasks');
+  console.log('POST /api/tasks');
 
   new Task(req.body)
   .save()
@@ -15,14 +15,14 @@ taskRouter.post('/api/tasks', jsonParser, (req, res, next) => {
 })
 
 taskRouter.put('/api/tasks/:id', jsonParser, (req, res, next) => {
-  console.log('/api/tasks/:id');
+  console.log('PUT /api/tasks/:id');
 
   let options = {
     new: true,
     runValidators: true
   }
 
-  Task.findByIdAndUpdate(req.params._id, req.body, options)
+  Task.findByIdAndUpdate(req.params.id, req.body, options)
   .then(task => res.json(task))
   .catch(next);
 })
