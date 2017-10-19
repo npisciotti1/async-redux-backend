caab75f192848ac0aaf6994ba152d2d851d67d33
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-19 16:00:56 -0700

    add PUT route for updating tasks

--- route/task-router.js
+++ route/task-router.js
@@ -13,3 +13,16 @@ taskRouter.post('/api/tasks', jsonParser, (req, res, next) => {
   .then( task => res.json(task))
   .catch(next);
 })
+
+taskRouter.put('/api/tasks/:id', jsonParser, (req, res, next) => {
+  console.log('/api/tasks/:id');
+
+  let options = {
+    new: true,
+    runValidators: true
+  }
+
+  Task.findByIdAndUpdate(req.params._id, req.body, options)
+  .then(task => res.json(task))
+  .catch(next);
+})
