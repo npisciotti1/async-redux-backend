1e7ab0d3f9715978ba95aa822a3712d30d4bfe7a
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-11-01 15:25:39 -0700

    comment out body-parser as isn't needed in analyze route

--- route/analyze-router.js
+++ route/analyze-router.js
@@ -1,24 +1,21 @@
 'use strict';
 
 const Tesseract = require('node-tesseract');
-const jsonParser = require('body-parser').json();
 const analyzeRouter = module.exports = new require('express').Router();
 
-const imgPath = `${__dirname}/../assets/movie.jpeg`
+// const imgPath = `${__dirname}/../assets/movie.jpeg`
 
 
-analyzeRouter.post('/api/analyze', jsonParser, (req, res, next) => {
+analyzeRouter.post('/api/analyze', (req, res, next) => {
   console.log('POST /api/analyze');
 
   console.log('got a request:', req.body);
 
-  Tesseract.process(imgPath, (err, text) => {
-    console.log(imgPath)
-    if(err) next(new Error('couldnt extract text'))
-
-
-    console.log(text);
-    res.json(text);
-  })
+  // Tesseract.process(req.body, (err, text) => {
+  //   if(err) next(new Error('couldnt extract text'))
+  //
+  //   console.log(text);
+  //   res.json(text);
+  // })
 
 })
