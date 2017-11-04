44f2615604e1139d8a2bce369c58a6a35773d3eb
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-11-03 18:12:23 -0700

    text analyze is actually working

--- route/analyze-router.js
+++ route/analyze-router.js
@@ -1,21 +1,31 @@
 'use strict';
 
+const fs = require('fs');
 const Tesseract = require('node-tesseract');
 const analyzeRouter = module.exports = new require('express').Router();
 
-// const imgPath = `${__dirname}/../assets/movie.jpeg`
 
 
 analyzeRouter.post('/api/analyze', (req, res, next) => {
   console.log('POST /api/analyze');
 
-  console.log('got a request:', req.body);
+  let imgPathWithExt = req.files.imageToExtract.path + '.jpeg';
+  fs.rename(req.files.imageToExtract.path, imgPathWithExt);
 
-  // Tesseract.process(req.body, (err, text) => {
-  //   if(err) next(new Error('couldnt extract text'))
-  //
-  //   console.log(text);
-  //   res.json(text);
-  // })
+  let thing = Tesseract.process(imgPathWithExt, (err, text) => {
+    if(err) return (new Error('couldnt extract text'));
+
+    sendRes(text);
+  })
+
+  function sendRes(text) {
+    res.json(text);
+
+    fs.unlink(imgPathWithExt, err => {
+      if(err) console.error('couldnt delete image')
+
+      console.log('image successfully deleted');
+    });
+  }
 
 })
