c6b8942a36ac04c978c38f1f3f9372e101ef12e3
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-11-06 11:34:34 -0800

    implement google-vision api

--- route/analyze-router.js
+++ route/analyze-router.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const fs = require('fs');
+const superagent = require('superagent');
 const analyzeRouter = module.exports = new require('express').Router();
+
 
 analyzeRouter.post('/api/analyze', (req, res, next) => {
   console.log('POST /api/analyze');
@@ -9,9 +11,34 @@ analyzeRouter.post('/api/analyze', (req, res, next) => {
   let imgPathWithExt = req.files.imageToExtract.path + '.jpeg';
   fs.rename(req.files.imageToExtract.path, imgPathWithExt);
 
-  fs.readFile(imgPathWithExt, (err, data) => {
+  let body = {
+    requests: [
+      {
+        image: {
+          content: null
+        },
+        features: [
+          {
+            type: "DOCUMENT_TEXT_DETECTION"
+          }
+        ]
+      }
+    ]
+  };
+
+  fs.readFile(imgPathWithExt, 'base64', (err, data) => {
     if (err) next(new Error('file not found'));
 
+    body.requests[0].image.content = data;
+
+    superagent.post(`https://vision.googleapis.com/v1/images:annotate`)
+    .query({key: process.env.GOOGLE_API_KEY})
+    .set('Content-Type', 'application/json')
+    .send(body)
+    .end((err, visionResult) => {
+      if(err) next(new Error('couldnt upload to Google Cloud Vision'))
+      res.send(visionResult.body);
+    })
   })
 
 });
