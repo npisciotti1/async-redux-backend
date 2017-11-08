a16ab600a69ddb0579ead5f6015064bfcd5e4241
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-11-08 12:13:01 -0800

    refactor route logic to function as middleware and separate module

--- route/analyze-router.js
+++ route/analyze-router.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const fs = require('fs');
-const superagent = require('superagent');
+const textExtract = require('../lib/text-extract');
+const imageParse = require('../lib/image-parse-middleware');
 const analyzeRouter = module.exports = new require('express').Router()
 
 analyzeRouter.get('/', (req, res, next) => {
@@ -9,44 +10,13 @@ analyzeRouter.get('/', (req, res, next) => {
 
 })
 
-analyzeRouter.post('/api/analyze', (req, res, next) => {
+analyzeRouter.post('/api/analyze', imageParse, (req, res, next) => {
   console.log('POST /api/analyze');
 
-  //build this out as middleware
-  let imgPathWithExt = req.files.imageToExtract.path + '.jpeg';
-  fs.rename(req.files.imageToExtract.path, imgPathWithExt);
+  textExtract(req.headers.imagePath)
+  .then(text => res.json(text))
+  .catch(next)
 
-  let body = {
-    requests: [
-      {
-        image: {
-          content: null
-        },
-        features: [
-          {
-            type: "DOCUMENT_TEXT_DETECTION"
-          }
-        ]
-      }
-    ]
-  };
-
-  fs.readFile(imgPathWithExt, 'base64', (err, data) => {
-    if (err) next(new Error('file not found'));
-
-    //set base64 data on Google Vision request body
-    body.requests[0].image.content = data;
-
-    superagent.post(`https://vision.googleapis.com/v1/images:annotate`)
-    .query({ key: process.env.GOOGLE_API_KEY })
-    .set('Content-Type', 'application/json')
-    .send(body)
-    .end((err, visionResult) => {
-      if(err) next(new Error('couldnt upload to Google Cloud Vision'))
-      res.status(200).json(visionResult.body);
-
-      //delete temp file
-      fs.unlink(imgPathWithExt, err => next(err))
-    })
-  })
+  //delete temp file
+  fs.unlink(req.headers.imagePath, err => next(err))
 });
