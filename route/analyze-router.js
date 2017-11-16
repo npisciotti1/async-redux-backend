1ce46fb04304d8fc54c687c3d78c8d23bfa17a41
npisciotti1 <nikkopisciotti@gmail.com>
2017-11-16 15:06:15 -0800

    remove bloat

--- route/analyze-router.js
+++ route/analyze-router.js
@@ -1,14 +1,8 @@
 'use strict';
 
-const fs = require('fs');
 const textExtract = require('../lib/text-extract');
 const imageParse = require('../lib/image-parse-middleware');
 const analyzeRouter = module.exports = new require('express').Router()
-
-analyzeRouter.get('/', (req, res, next) => {
-  res.status(200).json({cool: 'beans'});
-
-})
 
 analyzeRouter.post('/api/analyze', imageParse, (req, res, next) => {
   console.log('POST /api/analyze');
